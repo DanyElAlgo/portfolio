7dc8e9dc36131efd29e8c449c2ad0e2753168f18
DanyRoland <64906201+DanyElAlgo@users.noreply.github.com>
2025-05-30 20:28:23 -0400

    Update search-bar.js

--- components/search-bar.js
+++ components/search-bar.js
@@ -1,32 +1,3 @@
-// function SearchBar() {
-//   const content = `
-//         <div class="search-bar" id="search-bar">
-//           <input type="text" id="search-input">
-//         </div>
-//     `;
-//   render();
-// }
-
-// export default SearchBar;
-
-// function render() {
-//   globalThis.DOM = globalThis.DOM;
-//   DOM.searchBar = document.getElementById("search-bar");
-//   DOM.searchBar.innerHTML = "";
-
-//   const savedState = localStorage.getItem(`post_${post.id}`);
-//   const isSaved = savedState != null;
-
-//   const item = document.createElement("article");
-//   item.classList.add("blog__post");
-//   item.innerHTML = `
-//         <div class="search-bar" id="search-bar">
-//           <input type="text" id="search-input">
-//         </div>
-//     `;
-//   DOM.searchBar.appendChild(item);
-// }
-
 class SearchBar extends HTMLElement {
   constructor() {
     super();
@@ -42,12 +13,6 @@ class SearchBar extends HTMLElement {
     `;
 
     this.input = this.shadowRoot.getElementById('search-input');
-    this.input.addEventListener('input', this.handleInput.bind(this));
-  }
-
-  handleInput(event) {
-    const query = event.target.value;
-    this.dispatchEvent(new CustomEvent('search', { detail: query }));
   }
 }
 
